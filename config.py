91729b8f168e7434310801f896d3ff8370c8f365
marina <sadrinamarina88@gmail.com>
2026-01-31 23:57:14 +0500

    DB connected, token added

---
+++ config.py
@@ -0,0 +1,3 @@
+bot_token="8231168960:AAH6zpEZKFGcnMJgAbvAhPqZMNh_KCUBQKU"
+
+db_url="postgresql://postgres:0000@localhost:5432/postgres"
---
+++ main.py
@@ -0,0 +1,4 @@
+from config import bot_token, db_url
+
+print(bot_token)
+print(db_url)
